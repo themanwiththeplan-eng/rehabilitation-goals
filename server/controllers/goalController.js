e1c055a1547823a6badeef6fe0d8577f094587ad
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-25 14:54:48 -0700

    setting up routes and mongodb

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,24 +1,28 @@
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel');
+
 //function get goals at route GET/api/goals
-const getGoals = async (req, res) => {
-    res.status(200).json({ message: 'get goals'})
-}
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
+    res.status(404).json(goals)
+})
 
 //function set goal at route POST/api/goals
-const setGoal = async (req, res) => {
-    if (!req.body.text) {
-        res.status(404).json({ message: 'set a goal'})
-    }   
-}
+const setGoal = asyncHandler(async (req, res) => {
+        res.status(404).json({message: `Update goal ${req.params.id}`})
+    })   
+
 
 //function update goals at route PUT/api/goals/:id
-const updateGoal = async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(404).json({ message: `Update goal ${req.params.id}` })
+})
 
 //function delete goals at route delete/api/goals/:id
-const deleteGoal = async (req, res) => {
-    es.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(404).json({ message: `Delete goal ${req.params.id}` })
+})
 
 module.exports = {
     getGoals,
---
+++ server/models/goalModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.schema(
+    {
+        text: {
+            type: String,
+            required: true,
+        },
+    }, 
+    {
+        timestamps: true,
+    });
+
+module.exports = mongoose.model('Goal', goalSchema);    
