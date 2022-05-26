ed61f0b4e817bf66539e9dff5ab5f3a9696768d7
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-25 18:58:47 -0700

    add

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -10,13 +10,30 @@ const getGoals = asyncHandler(async (req, res) => {
 
 //function set goal at route POST/api/goals
 const setGoal = asyncHandler(async (req, res) => {
-        res.status(404).json({message: `Update goal ${req.params.id}`})
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Add goal')
+    }
+    const goal = await Goal.create({ 
+        text: req.body.text,
+    })
+        res.status(200).json(goal)
     })   
 
 
 //function update goals at route PUT/api/goals/:id
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(404).json({ message: `Update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(404)
+        throw new Error('No goal')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, 
+        { 
+        new: true,
+    })
+    res.status(200).json(updatedGoal)
 })
 
 //function delete goals at route delete/api/goals/:id
