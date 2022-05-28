c5e5b93722928b39095c73c637d292b8a2cba00c
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-28 10:51:07 -0700

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,9 @@
-// are we deleting this?
-const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb+srv://laurenshallop:password@cluster0.tpz22.mongodb.net/rehabgoals?retryWrites=true&w=majority`, {
+const mongoose = require('mongoose')
+mongoose.connect(
+  `mongodb+srv://laurenshallop:TpU7nsgF486b3do8@cluster0.tpz22.mongodb.net/rehabgoals?retryWrites=true&w=majority`,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true,
-});
-
-module.exports = mongoose.connection;
+  }
+)
+module.exports = mongoose.connection
